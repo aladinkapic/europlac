53dc6d40d5ddce64c464b48a819780cd9094dbc9
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2019-11-23 15:05:42 +0100

    Html collective

--- resources/views/layout/menu/top-menu.blade.php
+++ resources/views/layout/menu/top-menu.blade.php
@@ -1,3 +1,12 @@
 <div id="top-menu">
+    <div id="submenu-top">
+        <div id="menu-links">
+            <div class="logo-wrapper">
+                {!! Html::image(asset('/images/logo.png')) !!}
+            </div>
+            <div class="links-part">
 
+            </div>
+        </div>
+    </div>
 </div>
