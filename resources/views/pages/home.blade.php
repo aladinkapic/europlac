bcd3f11228ad4ec92bbc376c31ebe3c19e7a2479
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2019-12-07 18:26:23 +0100

    mobile version of slider

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,10 +8,78 @@
         <div class="swiper-container swiper2">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    {!! Html::image(asset('/images/slider/home-2.png')) !!}
+                    <img src="{{asset('/images/slider/home-2.png')}}" class="desktop-version" alt="">
+                    <img src="{{asset('/images/slider/home-m-1.png')}}" class="mobile-version" alt="">
+                    <div class="overlay-color"></div>
+                    <div class="slider-text">
+                        <h1>Brand New Luxury Condo in San Diego</h1>
+                        <div class="slider-properties">
+                            <div class="single-property">
+                                <div class="just-check">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                                <div class="just-property">
+                                    <h2>Stunning city and waterfront views</h2>
+                                </div>
+                            </div>
+                            <div class="single-property">
+                                <div class="just-check">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                                <div class="just-property">
+                                    <h2>Swedish hardwood flooring and Italian cabinetry</h2>
+                                </div>
+                            </div>
+                            <div class="single-property">
+                                <div class="just-check">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                                <div class="just-property">
+                                    <h2>Unparalleled services and amenities</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="slider-button">
+                            <a href="">
+                                <div class="slider-button-btn">
+                                    <p>ZAKAŽITE SASTANAK</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="swiper-slide">
-                    {!! Html::image(asset('/images/slider/home-2.png')) !!}
+                    <img src="{{asset('/images/slider/home-3.png')}}" class="desktop-version" alt="">
+                    <img src="{{asset('/images/slider/home-m-2.png')}}" class="mobile-version" alt="">
+                    <div class="overlay-color"></div>
+                    <div class="slider-text">
+                        <h1>Brand New Luxury Condo in San Diego</h1>
+                        <div class="slider-properties">
+                            <div class="single-property">
+                                <div class="just-check">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                                <div class="just-property">
+                                    <h2>Swedish hardwood flooring and Italian cabinetry</h2>
+                                </div>
+                            </div>
+                            <div class="single-property">
+                                <div class="just-check">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                                <div class="just-property">
+                                    <h2>Unparalleled services and amenities</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="slider-button">
+                            <a href="">
+                                <div class="slider-button-btn">
+                                    <p>ZAKAŽITE SASTANAK</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -31,11 +99,9 @@
                     /* type: 'progressbar', */
                     clickable: true,
                 },
-                breakpoints: {
-                    1200: {
-                        slidesPerView: 1,
-                    }
-                },
+                // autoplay: {
+                //     delay: 5000,
+                // },
                 navigation: {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
